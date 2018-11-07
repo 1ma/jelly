4f5503eac01c151ae6a7024db87befa951b98c3f
Marcel Hernandez <e6990620@gmail.com>
2018-11-07 22:23:44 +0100

    ServerCloak middleware

---
+++ src/Middleware/ServerCloak.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ABC\Middleware;
+
+use Psr\Http\Message;
+use Psr\Http\Server;
+
+/**
+ * A middleware that removes the X-Powered-By header from
+ * the response and overwrites Server with the value given
+ * at construction time.
+ */
+final class ServerCloak implements Server\MiddlewareInterface
+{
+    /**
+     * @var string
+     */
+    private $serverName;
+
+    public function __construct(string $serverName)
+    {
+        $this->serverName = $serverName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(Message\ServerRequestInterface $request, Server\RequestHandlerInterface $handler): Message\ResponseInterface
+    {
+        \header_remove('X-Powered-By');
+
+        return $handler->handle($request)
+            ->withHeader('Server', $this->serverName);
+    }
+}
