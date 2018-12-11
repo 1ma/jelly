715731dbb746501506f1cb61a6902f0aa7b7fd92
Marcel Hernandez <e6990620@gmail.com>
2018-12-11 23:53:21 +0100

    test Echoer

---
+++ tests/Output/FastCGIEchoerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ABC\Tests\Output;
+
+use ABC\Output\FastCGIEchoer;
+use Nyholm\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+final class FastCGIEchoerTest extends TestCase
+{
+    /**
+     * This test has to run in a separate process to avoid a
+     * "Cannot modify header information - headers already sent" error
+     *
+     * @runInSeparateProcess
+     */
+    public function testRenderOfSimpleResponse(): void
+    {
+        \ob_start();
+
+        FastCGIEchoer::echo(new Response(200, ['Content-Type' => ['text/plain'], 'Content-Length' => ['6']], 'Hello.'));
+
+        self::assertSame('Hello.', \ob_get_clean());
+
+        // Unfortunately this cannot be correctly tested in the CLI SAPI
+        // because the header and headers_list functions are both disabled.
+        self::assertSame([], \headers_list());
+    }
+}
